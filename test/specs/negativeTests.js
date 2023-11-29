3c0cb9fe0e345f7506f6888447851e391dc34acc
ezrachecketts <ezra.checketts6509@stu.mtec.edu>
2023-11-29 13:12:41 -0700

    Updated the negative test file

---
+++ test/specs/negativeTests.js
@@ -0,0 +1,56 @@
+import { expect } from '@wdio/globals'
+import LoginPage from '../pageobjects/loginPage.js'
+
+// Username is incorrect
+describe('The website', () => {
+    it('should give an error and not allow the login', async () => {
+        await LoginPage.open();
+        await LoginPage.login('standard', 'secret_sauce');
+        await expect(LoginPage.loginLogo).toBeExisting();
+    })
+})
+
+// Password is incorrect
+describe('The website', () => {
+    it('should give an error and not allow the login', async () => {
+        await LoginPage.open();
+        await LoginPage.login('error_user', 'secrets_sauces');
+        await expect(LoginPage.loginLogo).toBeExisting();
+    })
+})
+
+// Username and password are incorrect
+describe('The website', () => {
+    it('should give an error and not allow the login', async () => {
+        await LoginPage.open();
+        await LoginPage.login('locked_user', 'secret-sauce');
+        await expect(LoginPage.loginLogo).toBeExisting();
+    })
+})
+
+// No username input
+describe('The website', () => {
+    it('should give an error and not allow the login', async () => {
+        await LoginPage.open();
+        await LoginPage.login('', 'secret_sauce');
+        await expect(LoginPage.loginLogo).toBeExisting();
+    })
+})
+
+// No password input
+describe('The website', () => {
+    it('should give an error and not allow the login', async () => {
+        await LoginPage.open();
+        await LoginPage.login('problem_user', '');
+        await expect(LoginPage.loginLogo).toBeExisting();
+    })
+})
+
+// No username or password input
+describe('The website', () => {
+    it('should give an error and not allow the login', async () => {
+        await LoginPage.open();
+        await LoginPage.login('', '');
+        await expect(LoginPage.loginLogo).toBeExisting();
+    })
+})
